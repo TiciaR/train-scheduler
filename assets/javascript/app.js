79ace1d56e5576dc63e4c00746fac44228e19152
Ticia <ticiarodela@gmail.com>
2020-03-15 18:04:46 -0700

    save

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function ()  {
+    var firebaseConfig = {
+        apiKey: "AIzaSyC0-XoMa-lsdriGZ6_BEsrPl9wcbejNuX0",
+        authDomain: "train-scheduler-b61bd.firebaseapp.com",
+        databaseURL: "https://train-scheduler-b61bd.firebaseio.com",
+        projectId: "train-scheduler-b61bd",
+        storageBucket: "train-scheduler-b61bd.appspot.com",
+        messagingSenderId: "390313426052",
+        appId: "1:390313426052:web:ff57994b58c2ab96ddd6d1"
+      };
+
+      firebase.initializeApp(firebaseConfig);
+
+      var database = firebase.database();
+
+$("#addTrain").on("click", function(event) {
+    event.preventDefault();
+console.log("hello")
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})    
+
